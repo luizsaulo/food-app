6eaf98a114c0dd8fa7d7105fe4049e34147d74b6
Luiz <luizsaulo@yahoo.com.br>
2022-05-17 10:41:14 -0300

    adding the navbar component with its links and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function  Navbar(){
+    return(
+        <div className='navbar container'>
+            <a href='#!' className='logo'>F<span>oo</span>diesHub</a>
+            <div className='nav-links'>
+                <a href='#!'>Home</a>
+                <a href='#!'>Recipes</a>
+                <a href='#!'>Settings</a>
+            </div>
+        </div>
+    )
+}
