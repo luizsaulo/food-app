d756d66220e6bbaa4a77b16abf020bc3f4e3b6cf
Luiz <luizsaulo@yahoo.com.br>
2022-05-18 09:22:39 -0300

    building a hamburger icon from scratch and making it transform in an X with a usestate hook and onclick fucntion to make it active only when it´s clicked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 export default function  Navbar(){
+    const [showSidebar, setShowSidebar] = useState(false)
     return(
         <div className='navbar container'>
             <a href='#!' className='logo'>F<span>oo</span>diesHub</a>
@@ -6,6 +9,11 @@ export default function  Navbar(){
                 <a href='#!'>Home</a>
                 <a href='#!'>Recipes</a>
                 <a href='#!'>Settings</a>
+            </div>
+            <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? 'sidebar-btn active' : 'sidebar-btn'}>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
             </div>
         </div>
     )
