e97b4eb5714e656d599ef3ab9c91001d63532f99
Luiz <luizsaulo@yahoo.com.br>
2022-05-19 09:22:08 -0300

    adding a section that contains a photo gallery - this includes configuring the section and consuming images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className='container main'>
+        <HeroSection />
+      </div>
     </div>
   );
 }
