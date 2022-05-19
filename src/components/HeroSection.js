ef20f974b9c8fa1edf03c6c6d217faa2ff22ea00
Luiz <luizsaulo@yahoo.com.br>
2022-05-19 09:36:58 -0300

    adding a transform and effect on the gallery to make it spiral every time the page loads

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -25,7 +25,7 @@ export default function HeroSection() {
       </div>
       <div className="col gallery">
         {images.map((src, index) => (
-          <CustomImage key={index} imgSrc={src} pt={"85%"} />
+          <CustomImage key={index} imgSrc={src} pt={"90%"} />
         ))}
       </div>
     </div>
