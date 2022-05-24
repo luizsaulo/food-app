513857cead6feb4acdf4edfae02047744370a52a
Luiz <luizsaulo@yahoo.com.br>
2022-05-24 17:17:26 -0300

    working on the settings page adding the theme colors and preferred colors; still not completed

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
+import { useState } from "react";
 
 export default function Settings() {
+  const [theme, setTheme] = useState("light");
+  const primaryColors = [
+    "rgb(255, 0, 86)",
+    "rgb(33, 150, 243)",
+    "rgb(255, 193, 7)",
+    "rgb(0, 200, 83)",
+    "rgb(156, 39, 176)",
+  ]
+  const [primaryColor, setPrimaryColor] = useState(0)
   return (
-    <div>Settings</div>
-  )
+    <div>
+      <div className="section d-block">
+        <h2>Primary color</h2>
+        <div className="options-container">
+          <div className="option light">
+            {theme === "light" && (
+              <div className="check">
+                <FontAwesomeIcon icon={faCheck} />
+              </div>
+            )}
+          </div>
+          <div className="option dark">
+            {theme === "dark" && (
+              <div className="check">
+                <FontAwesomeIcon icon={faCheck} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="section d-block">
+        <h2>Preferred theme</h2>
+        <div className="options-container">          
+          {primaryColors.map((color, index) => (
+            <div className="option light" style={{backgroundColor: color}}>
+              { primaryColor == index && (
+                <div className="check">
+                  <FontAwesomeIcon icon={faCheck} />
+                </div>
+              ) }
+            </div>          
+          ))}            
+        </div>
+      </div>
+    </div>
+  );
 }
